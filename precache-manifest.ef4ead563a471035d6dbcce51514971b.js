bdf440deb33ae063dc475e388e719d8ff161c285
tjadhav <tusharjadhav@pm.me>
2019-07-30 12:37:17 +0530

    Updates

---
+++ precache-manifest.ef4ead563a471035d6dbcce51514971b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baaf686006080736032d15822d073a29",
+    "url": "/season-react/index.html"
+  },
+  {
+    "revision": "135fb5c476b3d34125b2",
+    "url": "/season-react/static/css/main.ad8d3fee.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/season-react/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "135fb5c476b3d34125b2",
+    "url": "/season-react/static/js/main.3e75ef0f.chunk.js"
+  },
+  {
+    "revision": "9cb0c0cdc6b5a3cc9c00",
+    "url": "/season-react/static/js/runtime~main.c3f53826.js"
+  }
+]);
